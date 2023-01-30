a62e0a83572eaa74acd54d17ee6e1bbdb0c9cdce
Sergi Casquero <sergicasquero@gmail.com>
2023-01-30 22:27:02 +0100

    add styles to gentlemen cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,21 @@ import gentlemenData from "./data/data";
 
 function App() {
   return (
-    <div className="App">
-      <ul>
-        {gentlemenData.map((gentleman) => (
-          <Gentleman gentleman={gentleman} key={gentleman.id} />
-        ))}
-      </ul>
+    <div className="container">
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
+      </header>
+      <section className="controls">
+        <p className="info">0 gentlemen pointing at you</p>
+        <button className="button button--select">Select all</button>
+      </section>
+      <main className="main">
+        <ul>
+          {gentlemenData.map((gentleman) => (
+            <Gentleman gentleman={gentleman} key={gentleman.id} />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
